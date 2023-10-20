6a744e7c1bcc9c91ef2acbb8d0cc5512c78c7199
s mugi <kuma.hati.labo@gmail.com>
2023-10-21 01:57:39 +0900

    test 実装中

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # coding-interview
+
+model を一部変更しました
+・Category - parent_category : blank=True を追加
+    理由：最初の Category を作れないので
+
+テスト時 WARNINGS が出力されます。これは開発環境に使った SQLite が db_comment に非対応なために出力されたものです。テスト自体には影響は有りません。
+
+
+|method|エンドポイント|名前|機能|
+| ---- | ---- | ---- | ---- |
+| GET  |/api/category/|一覧取得|全Category取得|
+| POST |/api/category/|新規作成|Category新規作成|
+| GET  |/api/category/{id}|個別取得|特定のCategoryを取得|
+| PUT  |/api/category/{id}|一部更新|特定のCategoryの内容を更新|
+|DELETE|/api/category/{id}|個別削除|特定のCategoryを削除|
+
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,11 +1,29 @@
-from rest_framework.test import APITestCase
+import json
 
+from rest_framework.test import APITestCase
+from ..models.category import Category
+from ..models.company import Company
 
 class CategoryViewTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        com = Company.objects.create(name="COT01")
+        Category.objects.create(name="CAT01", company=com)
+    
     def test_list(self):
+        response = self.client.get('/api/category/')
+        self.assertEqual(200, response.status_code)
+        payload = response.json()
+        print(payload)
+        com = Company.objects.get(name="COT01")
+        self.assertEqual(payload[0]["company"]["id"], str(com.id))
+
+#        self.assertEqual([], payload)
+               
         pass
 
     def test_retrieve(self):
+#        response = self.client.post('/api/category/',{'company': ''} )
         pass
 
     def test_create(self):
@@ -16,3 +34,4 @@ class CategoryViewTests(APITestCase):
 
     def test_destroy(self):
         pass
+
