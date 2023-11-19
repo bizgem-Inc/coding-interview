b0730f86a7215ec519bef3bec224fbbb9eb763f4
ryoutoku <invisible060@gmail.com>
2023-11-20 00:14:03 +0900

    feat: APIのURI設定

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import (category,
+                    company)
+
 router = DefaultRouter()
 
-urlpatterns = [path("", include(router.urls))]
+router.register(r'categories', category.CategoryViewSet)
+router.register(r'companies', company.CompanyViewSet)
+urlpatterns = [path(r'', include(router.urls)),]
